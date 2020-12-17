2a2f13ac00824cbd6268539ef0cbf833362b6b42
lajizaishi <879599011@qq.com>
2020-12-17 22:08:41 +0800

    redux完毕

--- aiya-client/src/redux/reducers.js
+++ aiya-client/src/redux/reducers.js
@@ -2,6 +2,7 @@
 *包含n个reducer函数：根据老的state和指定的action返回一个新的state
 * */
 import {combineReducers} from 'redux'
+import {AUTH_SUCCESS, ERROR_MSG} from "./action-types";
 
 const initUser = {
     username:'',//用户名
@@ -11,6 +12,10 @@ const initUser = {
 //产生user状态的reducer
 function user(state=initUser,action) {
     switch (action.type) {
+        case AUTH_SUCCESS:
+            return {...state,...action.data}
+        case ERROR_MSG:
+            return {...state,...action.data}
         default:
             return state
     }
